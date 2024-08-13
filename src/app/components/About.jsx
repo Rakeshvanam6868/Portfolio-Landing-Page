8a9457a7ba7c37f57e5c81e328fc66c9392139b4
Rakeshvanam6868 <rakeshvanam6868@gmail.com>
2024-08-13 23:23:49 +0530

    Checking About

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -8,16 +8,16 @@ const About = () => {
   return (
     <div className={`absolute w-[1470px] h-[675px] mt-[1004px] ml-[25px]`}>
       <div className={`absolute bg-orange-600 opacity-70 w-[374px] h-[83px] mt-[112px] ml-[303px] z-10`}></div>
-        <div className={`absolute w-[681px] h-[675px] ml-[150px] `}>
+        <div className={`absolute w-[681px] h-[675px] ml-[25px] `}>
             <Image
                 src={avatar}
                 alt=""
             />
         </div>
         <div className="flex items-start flex-col">
-            <p className={`absolute w-[330px] h-[98px] mt-[55px] ml-[854px] ${poppins.className} font-[600] text-[65px] `}>About Me</p>
-            <p className={`absolute w-[756px] h-[101px] mt-[168px] ml-[854px] ${poppins.className} font-[400] text-[21px]`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae cupiditate harum quia odio perferendis earum excepturi! Omnis id deserunt iure deleniti, ipsum quas </p>
-            <div className={`w-[753px] h-[349px] mt-[284px] ml-[854px] rounded-[5px]`}>
+            <p className={`absolute w-[330px] h-[98px] mt-[55px] ml-[654px] ${poppins.className} font-[600] text-[65px] `}>About Me</p>
+            <p className={`absolute w-[756px] h-[101px] mt-[168px] ml-[654px] ${poppins.className} font-[400] text-[21px]`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem quae cupiditate harum quia odio perferendis earum excepturi! Omnis id deserunt iure deleniti, ipsum quas </p>
+            <div className={`w-[753px] h-[349px] mt-[284px] ml-[654px] rounded-[5px]`}>
             <DesignHeading
           title="UX"
           width={34}
